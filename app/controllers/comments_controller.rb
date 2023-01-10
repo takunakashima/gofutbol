c6c49ec40882a36c45b789ec24704e3114b4c053
takunakashima <5002523tk@gmail.com>
2023-01-10 16:51:04 +0900

    コメントコントローラ実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
@@ -8,6 +9,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'posts#index'
   resources :posts  do
-    resources :comments, only: :create
+    resources :comments, only: [:create]
   end
 
-  root to: 'posts#index'
   end
