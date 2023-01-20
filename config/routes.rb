4eac1c42f078c7162826fb3f0d0d4314698301e0
takunakashima <5002523tk@gmail.com>
2023-01-20 18:15:39 +0900

    README

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  # resources :posts  do
   resources :posts, expect: [:index] do
     resources :comments, only: [:create]
     collection do
@@ -10,9 +9,7 @@ Rails.application.routes.draw do
   end
   post 'favorite/:id' => 'favorites#create', as: 'create_favorite'
   delete 'favorite/:id' => 'favorites#destroy', as: 'destroy_favorite'
-  # resources :users, only: :show
   resources :users, only: [:show, :edit, :update] do
-    # get :favorites, on: :collection
     
   end
 
